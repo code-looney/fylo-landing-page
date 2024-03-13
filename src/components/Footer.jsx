2b39fb3b2df05d148451cd56c045760dde835406
code-looney <danielvbofficieel@gmail.com>
2024-03-13 19:26:28 +0100

    created footer desk

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,18 +10,18 @@ const Footer = () => {
 
   return (
     <Container className='bg-sec text-white h-screen flex outline'>
-        <Container className='w-full flex flex-col gap-10 justify-center outline pl-5'>
+        <Container className='w-full flex flex-col gap-10 justify-center'>
           <Container className='w-52 flex translate-x-[20px]'>
             <Logo />
           </Container>
-          <Container className='flex gap-5'>
+          <Container className='flex gap-5 pl-5'>
             <Container className='w-5 relative h-full translate-y-[8px] '>
               <Icon iconURL='src/images/icon-location.svg' />
           </Container>
             <Paragraph className='font-thin'>Lorem ipsum dolor sit amet,<br className='block md:hidden' /> consectetur adipiscing elit, sed do<br className='block md:hidden' /> eiusmod tempor incididunt ut labore<br className='block md:hidden' /> et 
               dolore magna aliqua</Paragraph>
           </Container>
-          <Container className='flex flex-col gap-5'>
+          <Container className='flex flex-col gap-5 pl-5'>
               <Container className='flex gap-3 items-center'>
                 <Container className='w-[15px]'>
                   <Icon iconURL='src/images/icon-phone.svg' />
@@ -34,6 +34,24 @@ const Footer = () => {
                 </Container>
                 <Link>example@fylo.com</Link>
               </Container>
+          </Container>
+          <Container className='flex flex-col gap-10 pl-5'>
+            <Container className='flex flex-col'>
+                <Link href='#'>About Us</Link>
+                <Link href='#'>Jobs</Link>
+                <Link href='#'>Press</Link>
+                <Link href='#'>Blog</Link>
+          </Container>
+
+              <Container className='flex flex-col'>
+                <Link href='#'>Contact Us</Link>
+                <Link href='#'>Terms</Link>
+                <Link href='#'>Privacy</Link>
+              </Container>
+          </Container>
+
+          <Container className='flex w- h-[150px] items-center justify-center'>
+            <Socials socialClassName='flex gap-3' />
           </Container>
         </Container>
     </Container>
