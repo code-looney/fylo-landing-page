be48e298000653da95ca9205bcaef05890b77256
code-looney <danielvbofficieel@gmail.com>
2024-03-13 18:49:42 +0100

    working on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,49 +7,35 @@ import Link from './Link';
 import Socials from './Socials';
 
 const Footer = () => {
+
   return (
-    <Container className='bg-sec text-white h-screen md:h-[400px] flex flex-col justify-center items-center'>
-      <Container className='outline md:w-full '>
-        <Container className='w-96 md:w-full md:justify-around flex flex-col md:flex-row gap-10 outline'>
-          <Container className='md:absolute'>
-            <Logo className='w-40' />
+    <Container className='bg-sec text-white h-screen flex outline'>
+        <Container className='w-full flex flex-col gap-10 justify-center outline pl-5'>
+          <Container className='w-52 flex translate-x-[20px]'>
+            <Logo />
           </Container>
-          <Container className='flex gap-5 place-items-start'>
-            <Container className='pt-[5px]'>
-              <Icon iconClassName='w-[15px] md:w-[8px]' iconURL='src/images/icon-location.svg' />
-            </Container>
-            <Paragraph className='font-thin'>Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit, sed do <br /> eiusmod tempor incididunt ut labore <br /> et dolore magna aliqu</Paragraph>
+          <Container className='flex gap-5'>
+            <Container className='w-5 relative h-full translate-y-[8px] '>
+              <Icon iconURL='src/images/icon-location.svg' />
+          </Container>
+            <Paragraph className='font-thin'>Lorem ipsum dolor sit amet,<br className='block md:hidden' /> consectetur adipiscing elit, sed do<br className='block md:hidden' /> eiusmod tempor incididunt ut labore<br className='block md:hidden' /> et 
+              dolore magna aliqua</Paragraph>
           </Container>
           <Container className='flex flex-col gap-5'>
-            <Container className="flex gap-5">
-              <Container className='pt-[5px]'>
-                <Icon iconClassName='w-[15px] md:w-[8px]' iconURL='src/images/icon-phone.svg' />
+              <Container className='flex gap-3 items-center'>
+                <Container className='w-[15px]'>
+                  <Icon iconURL='src/images/icon-phone.svg' />
+                </Container>
+                <Link>+1-543-123-4567</Link>
               </Container>
-                <Paragraph>+1-543-123-4567</Paragraph>
-            </Container>
-            <Container className="flex gap-5">
-              <Container className='pt-[5px]'>
-                <Icon iconClassName='w-[17px] md:w-[8px]' iconURL='src/images/icon-email.svg' />
+              <Container className='flex gap-3 items-center'>
+                <Container className='w-[25px]'>
+                  <Icon iconURL='src/images/icon-email.svg' />
+                </Container>
+                <Link>example@fylo.com</Link>
               </Container>
-                <Paragraph>example@fylo.com</Paragraph>
-              </Container>
-            </Container>
-          <Container className='flex flex-col gap-3 font-thin'>
-            <Link className='hover:font-medium' href='#'>About Us</Link>
-            <Link className='hover:font-medium' href='#'>Jobs</Link>
-            <Link className='hover:font-medium' href='#'>Press</Link>
-            <Link className='hover:font-medium' href='#'>Blog</Link>
           </Container>
-          <Container className='flex flex-col gap-3 font-thin'>
-            <Link className='hover:font-medium' href='#'>Contact Us</Link>
-            <Link className='hover:font-medium' href='#'>Terms</Link>
-            <Link className='hover:font-medium' href='#'>Privacy</Link>
-          </Container>
-          <Container className='flex h-32 items-end md:items-start justify-center'>
-          <Socials socialClassName='flex gap-3' />
         </Container>
-        </Container>
-      </Container>
     </Container>
   )
 }
